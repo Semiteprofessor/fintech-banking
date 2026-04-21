f7362c55935cc13d8cd5d70981cfd338359b9f49
Taiwo Olapade <semiteprofessor@gmail.com>
2026-04-21 12:02:36 -0700

    Commit

---
+++ src/main/java/com/fintech/banking/repository/TransactionRepository.java
@@ -0,0 +1,13 @@
+package com.fintech.banking.repository;
+
+import com.fintech.banking.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, String> {
+
+    List<Transaction> findByUserIdOrderByCreatedAtDesc(String userId);
+}
